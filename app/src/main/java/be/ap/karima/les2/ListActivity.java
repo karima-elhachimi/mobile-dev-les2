19b0b8de3f7d5751ce17e5390ca14b2e8a4b3a8a
karima-elhachimi <yuakarima@Yuas-MacBook-Pro.local>
2018-09-20 11:19:21 +0200

    list activity aangemaakt en vanuit een intent mee verbonden

---
+++ app/src/main/java/be/ap/karima/les2/ListActivity.java
@@ -0,0 +1,23 @@
+package be.ap.karima.les2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class ListActivity extends android.app.ListActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+
+
+
+}
--- app/src/main/java/be/ap/karima/les2/MainActivity.java
+++ app/src/main/java/be/ap/karima/les2/MainActivity.java
@@ -1,8 +1,11 @@
 package be.ap.karima.les2;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,10 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
     private Button btn;
-
-    private ArrayList<String> list;
-    private StableArrayAdapter adapter;
-    private ListView listview = (ListView) findViewById(R.id.listView);
+    private Button listBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +33,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //setup voor alles van arrayadapter
-
-        this.listSetUp();
-
+        listBtn = (Button) findViewById(R.id.listButton);
+        listBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                goToList(v);
+            }
+        });
 
 
     }
@@ -50,41 +52,12 @@ public class MainActivity extends AppCompatActivity {
         new SimpleAsyncTask(mTextView).execute();
     }
 
-
-    private void listSetUp() {
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
-                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
-                "Android", "iPhone", "WindowsMobile" };
-
-        for (int i = 0; i < values.length; ++i) {
-            list.add(values[i]);
-        }
-
-        this.adapter = new StableArrayAdapter(this,
-                android.R.layout.simple_list_item_1, list);
-        this.listview.setAdapter(this.adapter);
-        this.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, final View view,
-                                    int position, long id) {
-                final String item = (String) parent.getItemAtPosition(position);
-                view.animate().setDuration(2000).alpha(0)
-                        .withEndAction(new Runnable() {
-                            @Override
-                            public void run() {
-                                list.remove(item);
-                                adapter.notifyDataSetChanged();
-                                view.setAlpha(1);
-                            }
-                        });
-            }
-
-        });
-
+    public void goToList(View view){
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
     }
+
+
 
 
 
